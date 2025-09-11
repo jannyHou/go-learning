45bf0ec795e9283c51edcc70c965dede4d711b50
Janny Hou <juehou2015@gmail.com>
2025-09-11 17:19:06 -0400

    update: loop v from channel

--- wg/wg.go
+++ wg/wg.go
@@ -1,39 +1,28 @@
 package wg
 
 import (
-	"context"
 	"fmt"
 	"sync"
 )
 
 func WgTest() {
 	var wg sync.WaitGroup
-	cb := context.Background()
-	ctx, cancelFun := context.WithCancel(cb)
 	ich := make(chan int)
 
-	wg.Add(1)
+	wg.Add(5)
 
 	go func() {
 		fmt.Println("go routine")
-		StageRunner(ctx, ich)
-		wg.Done()
+		for v := range ich {
+			fmt.Println(v)
+			wg.Done()
+		}
 	}()
+
+	for i := 0; i < 5; i++ {
+		ich <- i
+	}
 
 	wg.Wait()
 	fmt.Println("main")
-	cancelFun()
-
-}
-
-func StageRunner(c context.Context, ich chan int) {
-	fmt.Println("stage runner triggered")
-	for {
-		select {
-		case <-c.Done():
-			fmt.Println("Context cancelled")
-		case i, ok := <-ich:
-			fmt.Printf("ok is %v, value is %v\n", ok, i)
-		}
-	}
 }
