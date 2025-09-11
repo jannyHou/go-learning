c45e111cbd767973f7212c5d11b1d749924a7547
Janny Hou <juehou2015@gmail.com>
2025-09-11 16:53:20 -0400

    feat: reproduce deadlock

--- wg/wg.go
+++ wg/wg.go
@@ -1,19 +1,39 @@
 package wg
 
 import (
+	"context"
 	"fmt"
 	"sync"
 )
 
 func WgTest() {
 	var wg sync.WaitGroup
+	cb := context.Background()
+	ctx, cancelFun := context.WithCancel(cb)
+	ich := make(chan int)
+
+	wg.Add(1)
 
 	go func() {
 		fmt.Println("go routine")
+		StageRunner(ctx, ich)
 		wg.Done()
 	}()
-	wg.Add(1)
 
 	wg.Wait()
 	fmt.Println("main")
+	cancelFun()
+
+}
+
+func StageRunner(c context.Context, ich chan int) {
+	fmt.Println("stage runner triggered")
+	for {
+		select {
+		case <-c.Done():
+			fmt.Println("Context cancelled")
+		case i, ok := <-ich:
+			fmt.Printf("ok is %v, value is %v\n", ok, i)
+		}
+	}
 }
